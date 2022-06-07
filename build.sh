235ff571d30f85b3c6c2f431dc45e4a4dc3c423d
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-06-07 19:21:38 +0200

    added files

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Maintainer: Mahmoud Mohamed (Ozil) <mmsaeed509@gmail.com> , <https://github.com/mmsaeed509>
+# contributor :  Omar Esmail <https://github.com/OmarEsmail211>
+#
+
+echo ""
+echo "######################"
+echo "# Making the package #"
+echo "######################"
+echo ""
+
+makepkg -sc --noconfirm
+yes| paru -c
+mkdir -p ../core-x86_64
+mv *.pkg.tar.zst ../core-x86_64
+
+echo ""
+echo "########################################"
+echo "# Moved the .zst file to x86_64 folder #"
+echo "########################################"
+echo ""
+
+find . -type d -not -path "." -not -path "./files" -exec sudo rm -r "{}" \;
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -not -name "*.hook" -not -name "exodia-hooks-runner" -delete
+
+echo ""
+echo "###########################################"
+echo "# Removed all redundant files and folders #"
+echo "###########################################"
+echo ""
